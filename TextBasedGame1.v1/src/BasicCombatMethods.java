dac9605ab55f9474ee350fe2786ae5da465404af
MachoMusclesSteroidSnakeTucker <87863444+MachoMusclesSteroidSnakeTucker@users.noreply.github.com>
2022-04-23 13:53:24 +0200

    Delete TextBasedGame1.v1 directory

--- TextBasedGame1.v1/src/BasicCombatMethods.java
+++
@@ -1,7 +0,0 @@
-
-public interface BasicCombatMethods {
-
-	int attack(int attack, int eDefense, String name);
-	int defend(int defense, int eAttack);
-	
-}
--- TextBasedGame1.v1/src/Stvore.java
+++
@@ -1,7 +0,0 @@
-
-public class Stvore {
-
-	int maxHealth, health, attack, defense, agility;
-	String name;
-	
-}
